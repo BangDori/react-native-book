96eac786be9662e1b8e07dda07542aca488328b6
ByungJun Kang <44726494+BangDori@users.noreply.github.com>
2023-05-15 16:23:05 +0900

    투두리스트 lll

---
+++ rn-todo/src/components/EmptyList.js
@@ -0,0 +1,32 @@
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { PRIMARY } from '../colors';
+import main from '../../assets/main.png';
+
+const EmptyList = () => {
+  return (
+    <View style={styles.container}>
+      <Image source={main} style={styles.image} />
+      <Text style={styles.text}>할 일을 추가해주세요.</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+  text: {
+    marginTop: 10,
+    color: PRIMARY.DARK,
+    fontSize: 18,
+    fontWeight: '700',
+  },
+});
+
+export default EmptyList;
